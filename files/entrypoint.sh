829e56ce9fe399028937a3e6a68c3534b731a42b
coders-lair <bbec@coders-lair.dev>
2024-08-25 02:06:27 +0200

    [fix] readonly mount contents cannot be changed by chown

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -8,14 +8,10 @@ _term_trigger() {
 
 trap _term_trigger SIGTERM
 
-chown -R git:git /home/git /srv/git
 ln -sf /srv/git /home/git/repos
 ln -sf /srv/git /repos
 
-for key_file in /home/git/.ssh/authorized_keys.d/*; do
-  cat $key_file;
-  echo
-done >/home/git/.ssh/authorized_keys
+find /home/git/.ssh/authorized_keys.d/ -type f -exec cat \{\} \; -exec echo \; >/home/git/.ssh/authorized_keys
 
 "$@" &
 
